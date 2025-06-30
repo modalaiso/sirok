52fd0fec48e3acdd450ff7034b39eda1cae4625c
modalaiso <mobioisrael@hotmail.com>
2025-06-30 23:19:38 +0000

    Update carrousel.js

--- frontend/js/carrousel.js
+++ frontend/js/carrousel.js
@@ -1,17 +1,16 @@
-
 const track = document.querySelector('.carousel-track');
 
 // Pause l'animation quand on survole
-//track.addEventListener('mouseenter', () => {
-    //track.style.animationPlayState = 'paused';
-//});
+track.addEventListener('mouseenter', () => {
+    track.style.animationPlayState = 'paused';
+});
 
-//track.addEventListener('mouseleave', () => {
-    //track.style.animationPlayState = 'running';
-//});
+track.addEventListener('mouseleave', () => {
+    track.style.animationPlayState = 'running';
+});
 
 // Permet le scroll manuel horizontal (optionnel si besoin)
-//track.style.cursor = 'grab';
+track.style.cursor = 'grab';
 let isDown = false;
 let startX, scrollLeft;
 
@@ -19,17 +18,17 @@ track.addEventListener('mousedown', (e) => {
     isDown = true;
     startX = e.pageX - track.offsetLeft;
     scrollLeft = track.scrollLeft;
-    //track.style.cursor = 'grabbing';
+    track.style.cursor = 'grabbing';
 });
 
 track.addEventListener('mouseleave', () => {
     isDown = false;
-    //track.style.cursor = 'grab';
+    track.style.cursor = 'grab';
 });
 
 track.addEventListener('mouseup', () => {
     isDown = false;
-    //track.style.cursor = 'grab';
+    track.style.cursor = 'grab';
 });
 
 track.addEventListener('mousemove', (e) => {
