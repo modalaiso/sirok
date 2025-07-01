847d87ee2aff9c356cbb5c529673eeed9f9d3ed3
modalaiso <mobioisrael@hotmail.com>
2025-07-01 13:30:59 +0000

    update

---
+++ frontend/js/reveal.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const reveals = document.querySelectorAll('.reveal');
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // pour ne l’animer qu’une fois
+        }
+      });
+    }, { threshold: 0.2 });
+  
+    reveals.forEach(card => observer.observe(card));
+  });
+  
